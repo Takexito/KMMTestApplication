4923c487350f8a52504bc2239182d44de6394c0f
Takexito <Takexito@gmail.com>
2023-01-19 11:55:10 +0300

    add FlowWrapper.kt

--- shared/src/commonMain/kotlin/com/example/kmmtestapplication/flow_test/ViewModel.kt
+++ shared/src/commonMain/kotlin/com/example/kmmtestapplication/flow_test/ViewModel.kt
@@ -5,7 +5,11 @@ import com.example.kmmtestapplication.flow.NativeFlow
 import com.example.kmmtestapplication.flow.Optional
 import com.example.kmmtestapplication.flow.asNativeFlow
 import com.example.kmmtestapplication.property.scope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 data class Player(
     val name: String,
@@ -23,6 +27,17 @@ class ViewModel(private val repository: Repository) {
     // Example with NativeFlow - lambda functions and 2 fields
     val player: MutableSharedFlow<Player>
         get() = repository.getPlayer()
+
+    val stateFlow: MutableStateFlow<Int?> = MutableStateFlow<Int?>(0)
+
+    init {
+        scope.launch {
+            delay(500)
+            stateFlow.emit(10)
+            delay(1000)
+            stateFlow.emit(null)
+        }
+    }
 
     val playerNativeFlow: NativeFlow<Player>
         get() = player.asNativeFlow()
